67102cdcc7175a61b5d1ffbcc2f64a2e2135892b
azureaws <devopstest1428@gmail.com>
2023-12-06 14:38:02 -0500

    Cat deployment

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# deploy.sh
+
+# Check if the environment parameter is provided
+if [ -z "$1" ]; then
+    echo "Error: Please provide the environment (e.g., cat or prod)"
+    exit 1
+fi
+
+ENVIRONMENT=$1
+
+echo "Deploying to $ENVIRONMENT environment"
+
+# Add deployment steps for the specified environment
+if [ "$ENVIRONMENT" == "cat" ]; then
+    # Add cat deployment steps here
+    echo "Deploying to cat environment..."
+    
+    # Git commands for cat deployment
+    echo "Adding changes for cat environment..."
+    git add .
+    git commit -m "Cat deployment"
+    git push origin cat  # Assuming you have a 'cat' branch for cat environment
+    
+elif [ "$ENVIRONMENT" == "prod" ]; then
+    # Add prod deployment steps here (excluding Helm folder)
+    echo "Deploying to prod environment..."
+    
+    # Exclude Helm folder during production deployment
+    echo "Excluding Helm folder for production deployment..."
+    rsync -a --exclude='Helm/' source/ destination/  # Adjust source and destination paths
+    
+    # Git commands for production deployment
+    echo "Adding changes for prod environment..."
+    git add .
+    git commit -m "Production deployment"
+    git push origin master  # Assuming you are pushing to the master branch
+else
+    echo "Error: Invalid environment specified"
+    exit 1
+fi
+
+echo "Deployment completed successfully"
